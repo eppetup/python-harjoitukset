1bb93db534d49f13c68a87c8bede93b6741cd159
Elias Tuohimaa <tuohimaa.elias@gmail.com>
2026-08-27 14:46:32 +0300

    tuntitehtävä laskin

---
+++ tuntitehtävät/mod05/laskin.py
@@ -0,0 +1,19 @@
+while(True):
+
+    print("hei, tämä on laskin. valitse toiminto:")
+    print("plus, miinus, kerto, seis")
+    toiminto = input("> ")
+
+    if toiminto == "seis":
+        quit()
+
+    luku1 = float(input("Anna 1. luku > "))
+    luku2 = float(input("Anna 2. luku > "))
+
+    if toiminto == "plus":
+        print(f"\n{luku1} + {luku2} = {luku1 + luku2}\n")
+    if toiminto == "miinus":
+            print(f"\n{luku1} - {luku2} = {luku1 - luku2}\n")    
+    if toiminto == "kerto":
+                print(f"\n{luku1} * {luku2} = {luku1 * luku2}\n") 
+

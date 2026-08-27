bfcbb9cbce2ac1111bd03966d4d197352eb32fe8
Elias Tuohimaa <tuohimaa.elias@gmail.com>
2026-08-27 13:59:49 +0300

    add virhe print

--- mod04/3.py
+++ mod04/3.py
@@ -4,10 +4,13 @@ hemoglobiinit = int(input("Anna hemoglobiiniarvo g/l > "))
 if sukupuoli == "nainen":
   alaraja = 117
   ylaraja = 175
-if sukupuoli == "mies":
+elif sukupuoli == "mies":
   alaraja = 134
   ylaraja = 195
-  
+else:
+  print("Virheellinen sukupuoli")
+  quit()
+
 if hemoglobiinit < alaraja:
   print("Hemoglobiiniarvo on alhainen")
 elif hemoglobiinit > ylaraja:

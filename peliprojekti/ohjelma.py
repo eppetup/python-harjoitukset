15e9358c931f23c8c8c8f33a7e5f4bc9b66db99e
Elias Tuohimaa <tuohimaa.elias@gmail.com>
2026-09-03 16:54:17 +0300

    peliprojekti

--- peliprojekti/ohjelma.py
+++ peliprojekti/ohjelma.py
@@ -1,4 +1,22 @@
 nimi = input('Pelaajan nimi > ')
 ika = input('Pelaajan ikä > ')
 
-print(f'Nimi: {nimi}\nIkä: {ika}')
+if int(ika) < 12:
+    print('Alaikä')
+    quit()
+
+while(True):
+
+    print(f'\nHei {nimi}!')
+    print('Valitse toiminto:')
+    print('\n  pelaa\n  asetukset\n  lopeta\n')
+
+    syote = input('> ')
+
+    match syote:
+        case 'pelaa':
+            print('*** peli työn alla ***')
+        case 'asetukset':
+            print('*** asetukset työn alla ***')
+        case 'lopeta':
+            quit()
